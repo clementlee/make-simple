54c2b3de91768bafdfd96235468fc8881a56408e
Clement Lee <clem@princeton.edu>
2013-11-09 01:19:17 -0500

    Something's working right

--- makesimple.js
+++ makesimple.js
@@ -7,19 +7,15 @@ xhr.onreadystatechange = function()
     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
     { 
         var temptextdatabase = xhr.responseText.split("\n");
-        console.log(temptextdatabase);
+        //console.log(temptextdatabase);
         for(var i = 0; i < temptextdatabase.length; i++) {
         	var line = temptextdatabase[i];
         	textdatabase.push(line.split(","));
         }
         walk(document.body);
-
-		document.body.addEventListener('DOMNodeInserted', function(event) {
-    		walk(event.target);});
     }
 };
 xhr.send();
-walk(document.body);
 document.body.addEventListener('DOMNodeInserted', function(event) {
     		walk(event.target);});
 
@@ -27,11 +23,13 @@ function trim1 (str) {
     return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
 function getSynonyms(word) {
-	if(typeof word != 'undefined')
+	if(typeof word === 'undefined')
 		return [word];
 	//TODO: FIX THIS BECAUSE IT ISN'T WORKING
 	for(var i = 0; i < textdatabase.length; i++ ) {
+		console.log("hi");
 		var line = textdatabase[i];
+		console.log(line);
 		for(var j = 0; j < line.length; j++) {
 			var test = line[j];
 			if(trim1(test)===trim1(word)) {
@@ -67,13 +65,13 @@ function process(text){
 	//console.log(words);
 	for(var i = 0; i < words.length; i++) {
 		var word = words[i];
-		if(typeof word!= 'undefined' && word.length > 8) {
-			console.log(word.length+": "+word);
+		if(typeof word != 'undefined' && word.length > 8) {
+			//console.log(word.length+": "+word);
 			var synonyms = getSynonyms(word);
 			//console.log(word+": ");
 			//console.log(synonyms);
-			if(synonyms != undefined) {
-				console.log("replacing "+word+" with "+ synonyms[0]);
+			if(typeof synonyms != 'undefined') {
+				//console.log("replacing "+word+" with "+ synonyms[0]);
 				processedText = processedText.replace(word, synonyms[0]);
 			}
 		}
