481837f5df17033cbdd67caef2281b1b27a29902
Clement Lee <clem@princeton.edu>
2013-11-08 23:37:12 -0500

    Broken commit, but there's been a lot of code added

--- makesimple.js
+++ makesimple.js
@@ -1,3 +1,31 @@
+var textdatabase = [];
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.extension.getURL('mthesaur.txt'), true);
+xhr.onreadystatechange = function()
+{
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+    {
+        var textdatabase = xhr.responseText.split("\n");
+        walk(document.body);
+    }
+};
+xhr.send();
+
+function getSynonyms(word) {
+	if(word == undefined)
+		return word;
+	console.log(word);
+	for(var i = 0; i < textdatabase.length; i++) {
+		var split = textdatabase[i].split(",");
+		for(var j = 0; j < split.length; j++)
+			if(split[i]===word)
+				return split;
+	}
+	return [word];
+}
+//DELETE THIS ASAP******************************************
+
 walk(document.body);
 
 document.body.addEventListener('DOMNodeInserted', function(event) {
@@ -32,7 +60,13 @@ function process(text){
 	var processedText = text;
 	var words = text.split(" ");
 	for(var i = 0; i < words.length; i++) {
-
+		var word = words[i];
+		if(word != undefined && word.length > 8)
+			var synonyms = getSynonyms(word);
+			//console.log(word+": ");
+			//console.log(synonyms);
+			if(synonyms != undefined)
+				processedText = processedText.replace(word, getSynonyms(word)[3]);
 	}
 	return processedText;
 }
---
+++ thesaurus.js
@@ -0,0 +1,18 @@
+var textdatabase;
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.extension.getURL('mthesaur.txt'), true);
+xhr.onreadystatechange = function()
+{
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+    {
+        textdatabase = xhr.responseText.split("\n");
+    }
+};
+xhr.send();
+
+function getSynonyms(word) {
+	for(var i = 0; i < textdatabase.length; i++)
+		if(textdatabase[i].indexOf(word) != -1)
+			return textdatabase[i].split(",");
+}
