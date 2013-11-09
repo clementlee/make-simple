25af0c2ee460bb6a16623dba802fc8adf35d24bf
Clement Lee <clem@princeton.edu>
2013-11-09 02:15:21 -0500

    I mean, it technically works...

--- makesimple.js
+++ makesimple.js
@@ -1,32 +1,44 @@
 var textdatabase = [];
+var hashtable = {};
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', chrome.extension.getURL('mthesaur.txt'), true);
 xhr.onreadystatechange = function()
 {
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-    { 
-        var temptextdatabase = xhr.responseText.split("\n");
+	if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+	{ 
+		var temptextdatabase = xhr.responseText.split("\n");
         //console.log(temptextdatabase);
         for(var i = 0; i < temptextdatabase.length; i++) {
-        	var line = temptextdatabase[i];
-        	textdatabase.push(line.split(","));
+        	var line = temptextdatabase[i].split(",");
+        	textdatabase.push(line);
+        	for(var j = 0; j < line.length; j++) {
+        		if(line[j].length > 4) {
+        			for(var k = 0; k < line.length; k++) {
+        				if(line[k].length < 5) {
+        					hashtable[line[j]] = line[k];
+        					break;
+        				}
+        			}
+        		}
+        	}
         }
         walk(document.body);
     }
 };
 xhr.send();
 document.body.addEventListener('DOMNodeInserted', function(event) {
-    		walk(event.target);});
+	walk(event.target);});
 
 function trim1 (str) {
-    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+	return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
 function getSynonyms(word) {
 	if(typeof word === 'undefined')
 		return [word];
+	word = word.toLowerCase();
 	//TODO: FIX THIS BECAUSE IT ISN'T WORKING
-	for(var i = 0; i < textdatabase.length; i++ ) {
+	/*for(var i = 0; i < textdatabase.length; i++ ) {
 		var line = textdatabase[i];
 		//console.log(line);
 		for(var j = 0; j < line.length; j++) {
@@ -36,7 +48,9 @@ function getSynonyms(word) {
 				return line;
 			}
 		}
-	}
+	}*/
+	if(hashtable.hasOwnProperty(word))
+		return [hashtable[word]];
 	return [word];
 }
 //DELETE THIS ASAP******************************************
@@ -45,11 +59,11 @@ function getSynonyms(word) {
 function walk(node) {
 	//console.log(textdatabase[0]);
 	var treeWalker = document.createTreeWalker(
-	    node,
-	    NodeFilter.SHOW_TEXT,
-	    null,
-	    false
-	);
+		node,
+		NodeFilter.SHOW_TEXT,
+		null,
+		false
+		);
 	while(treeWalker.nextNode()) {
 		var current = treeWalker.currentNode;
 		current.textContent = process(current.textContent);
@@ -64,7 +78,7 @@ function process(text){
 	//console.log(words);
 	for(var i = 0; i < words.length; i++) {
 		var word = words[i];
-		if(typeof word != 'undefined' && word.length > 5) {
+		if(typeof word != 'undefined' && word.length > 4) {
 			//console.log(word.length+": "+word);
 			var synonyms = getSynonyms(word);
 			//console.log(word+": ");
