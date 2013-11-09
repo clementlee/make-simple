c7b9682511ffc9b6734295ff2d4a78275e9f8166
Clement Lee <clem@princeton.edu>
2013-11-08 20:48:16 -0500

    Added basic architecture and downloaded jquery.js

---
+++ makesimple.js
@@ -0,0 +1,14 @@
+console.log("loading");
+
+simplify(document.body);
+function simplify(node) {
+	console.log("yo dawg");
+	var child;
+	switch(node.nodeType) {
+		case 1:
+			break;
+		case 3:
+			newwindow=window.open(url,'name','height=400,width=200');
+			break;
+	}
+}
