54278efb832f8182834c6d7a63005dc9c24f4255
Clement Lee <clem@princeton.edu>
2013-11-09 12:16:17 -0500

    Cleaned up a lot

--- makesimple.js
+++ makesimple.js
@@ -9,10 +9,12 @@ xhr.onreadystatechange = function()
 	if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
 	{ 
 		var temptextdatabase = xhr.responseText.split("\n");
-        //console.log(temptextdatabase);
+
         for(var i = 0; i < temptextdatabase.length; i++) {
         	var line = temptextdatabase[i].split(",");
+
         	textdatabase.push(line);
+
         	for(var j = 0; j < line.length; j++) {
         		if(line[j].length > aggression) {
         			for(var k = 0; k < line.length; k++) {
@@ -28,43 +30,30 @@ xhr.onreadystatechange = function()
     }
 };
 xhr.send();
+
 document.body.addEventListener('DOMNodeInserted', function(event) {
 	walk(event.target);});
 
-function trim1 (str) {
+function trim (str) {
 	return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
+
 function getSynonyms(word) {
 	if(typeof word === 'undefined')
 		return [word];
+
 	word = word.toLowerCase();
-	//TODO: FIX THIS BECAUSE IT ISN'T WORKING
-	/*for(var i = 0; i < textdatabase.length; i++ ) {
-		var line = textdatabase[i];
-		//console.log(line);
-		for(var j = 0; j < line.length; j++) {
-			var test = line[j];
-			if(trim1(test)===trim1(word)) {
-				console.log("found: "+test+", "+word.length);
-				return line;
-			}
-		}
-	}*/
+
 	if(hashtable.hasOwnProperty(word))
 		return [hashtable[word]];
+
 	return [word];
 }
-//DELETE THIS ASAP******************************************
-
 
 function walk(node) {
-	//console.log(textdatabase[0]);
 	var ent = document.createTreeWalker(
-		node,
-		NodeFilter.SHOW_TEXT,
-		null,
-		false
-		);
+		node, NodeFilter.SHOW_TEXT, null, false);
+
 	while(ent.nextNode()) {
 		var current = ent.currentNode;
 		current.textContent = process(current.textContent);
@@ -76,17 +65,20 @@ function process(text){
 	var processedText = text;
 	var words = text.split(" ");
 	
-	//console.log(words);
 	for(var i = 0; i < words.length; i++) {
 		var word = words[i];
+
 		if(typeof word != 'undefined' && word.length > aggression) {
-			//console.log(word.length+": "+word);
+
 			var synonyms = getSynonyms(word);
-			//console.log(word+": ");
-			//console.log(synonyms);
+
 			if(typeof synonyms != 'undefined') {
-				//console.log("replacing "+word+" with "+ synonyms[0]);
-				processedText = processedText.replace(word, synonyms[0]);
+				for(var j = 0; j < synonyms.length; j++){
+					if(synonyms[j].length <= aggression) {
+						processedText = processedText.replace(word, synonyms[j]);
+						break;
+					}
+				}
 			}
 		}
 	}
